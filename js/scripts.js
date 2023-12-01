8d66a741fd54c4fa758b63902ba4c9d3da6848ea
fredforthewin <150601495+fredforthewin@users.noreply.github.com>
2023-11-30 22:58:11 -0500

    added some js, fixed a tiny bit of how terrible it looks with css mostly

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
 console.log("hello world");
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
